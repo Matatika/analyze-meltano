98540542a53736cc398bfd7c10a7d78edba14566
Daniel Walker <34437496+DanielPDWalker@users.noreply.github.com>
2022-02-23 11:41:12 +0000

    Bumped version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-meltano",
-    version="0.1.1",
+    version="0.3.0",
     description="Meltano project file bundle of Matatika datasets for tap-meltano",
     packages=find_packages(),
     package_data={
