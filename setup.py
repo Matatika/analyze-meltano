4267a4aae08fb25960337585c4dd7aaf4e97a6be
danielpdwalker <danielpdwalker@gmail.com>
2022-03-21 10:51:51 +0000

    Bumped package version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-meltano",
-    version="0.3.0",
+    version="0.3.1",
     description="Meltano project file bundle of Matatika datasets for tap-meltano",
     packages=find_packages(),
     package_data={
